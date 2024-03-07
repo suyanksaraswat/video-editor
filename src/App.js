5549d3f35c2181a6ef90db3b603aaba9c9357e2f
Suyank Saraswat <suyank.saraswat@gmail.com>
2024-03-07 12:43:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import VideoEditor from "./components/VideoEditor"
+
+function App() {
+    return (
+        <div className={"app"}>
+            <VideoEditor />
+        </div>
+    )
+}
+
+export default App
